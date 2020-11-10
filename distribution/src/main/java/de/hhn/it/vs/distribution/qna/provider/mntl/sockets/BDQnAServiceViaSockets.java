3b8dfee525d829d61df984db2a2a419dfd2290ce
TLeibl <tabea.leibl@web.de>
2020-11-10 12:42:20 +0100

    began client implementation

--- distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/mntl/sockets/BDQnAServiceViaSockets.java
+++ distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/mntl/sockets/BDQnAServiceViaSockets.java
@@ -1,18 +1,28 @@
 package de.hhn.it.vs.distribution.qna.provider.mntl.sockets;
 
+import de.hhn.it.vs.common.core.usermanagement.Token;
+import de.hhn.it.vs.common.exceptions.IllegalParameterException;
+import de.hhn.it.vs.common.exceptions.InvalidTokenException;
 import de.hhn.it.vs.common.exceptions.ServiceNotAvailableException;
+import de.hhn.it.vs.common.qna.model.Answer;
+import de.hhn.it.vs.common.qna.model.Area;
+import de.hhn.it.vs.common.qna.model.Question;
+import de.hhn.it.vs.common.qna.service.BDQnAService;
+import de.hhn.it.vs.distribution.sockets.Request;
+import de.hhn.it.vs.distribution.sockets.Response;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.List;
 
 /**
  * Class Service Via Sockets
  * @author Leibl, Nauendorf
  * @version 2020-11-10
  */
-public class BDQnAServiceViaSockets {
+public class BDQnAServiceViaSockets implements BDQnAService {
 
     //logger for information during use
     private static final org.slf4j.Logger logger =
@@ -34,7 +44,7 @@ public class BDQnAServiceViaSockets {
     /*
     method to connect to the service
      */
-    private void connect() throws ServiceNotAvailableException {
+    private void connectToService() throws ServiceNotAvailableException {
         try {
             socket = new Socket(hostName, portNumber);
             out = new ObjectOutputStream(socket.getOutputStream());
@@ -49,7 +59,7 @@ public class BDQnAServiceViaSockets {
     /*
     method to disconnect from the service
      */
-    private void disconnect() {
+    private void disconnectFromService() {
         try {
             if (out != null) {
                 out.close();
@@ -70,4 +80,109 @@ public class BDQnAServiceViaSockets {
         }
     }
 
+    private Response sendAndGetResponse(final Request request) throws ServiceNotAvailableException {
+        // send request and wait for the response
+        try {
+            connectToService();
+            logger.debug("sending request " + request);
+            out.writeObject(request);
+            logger.debug("wait for response ...");
+            Response response = (Response) in.readObject();
+            logger.debug("Got response " + response);
+            disconnectFromService();
+            return response;
+        } catch (Exception ex) {
+            throw new ServiceNotAvailableException("Communication problem: " + ex.getMessage(), ex);
+        }
+    }
+
+
+    private void rethrowStandardExceptions(final Response response) throws
+        IllegalParameterException, ServiceNotAvailableException, InvalidTokenException {
+        Exception exceptionFromRemote = response.getExceptionObject();
+
+        // check all acceptable exception types and rethrow
+        if (exceptionFromRemote instanceof IllegalParameterException) {
+            throw (IllegalParameterException) exceptionFromRemote;
+        }
+
+        if (exceptionFromRemote instanceof InvalidTokenException) {
+            throw (InvalidTokenException) exceptionFromRemote;
+        }
+
+        if (exceptionFromRemote instanceof ServiceNotAvailableException) {
+            throw (ServiceNotAvailableException) exceptionFromRemote;
+        }
+
+        rethrowUnexpectedException(exceptionFromRemote);
+    }
+
+    private void rethrowUnexpectedException(final Exception exceptionFromRemote) throws
+        ServiceNotAvailableException {
+        // if we reach this part, than the exception is an exception we do not expect!
+        logger.error("Unknown exception object in response: " + exceptionFromRemote);
+        exceptionFromRemote.printStackTrace();
+        throw new ServiceNotAvailableException("Unknown exception received in response object.",
+            exceptionFromRemote);
+    }
+
+    @Override
+    public long createArea(Token userToken, Area area) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return 0;
+    }
+
+    @Override
+    public long createQuestion(Token userToken, long areaId, Question question) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return 0;
+    }
+
+    @Override
+    public long createAnswer(Token userToken, long areaId, long questionId, Answer answer) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return 0;
+    }
+
+    @Override
+    public List<Long> getAreaIds(Token userToken) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return null;
+    }
+
+    @Override
+    public Area getArea(Token userToken, long areaId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return null;
+    }
+
+    @Override
+    public List<Long> getQuestionIds(Token userToken, long areaId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return null;
+    }
+
+    @Override
+    public Question getQuestion(Token userToken, long areaId, long questionId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return null;
+    }
+
+    @Override
+    public List<Long> getAnswerIds(Token userToken, long areaId, long questionId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return null;
+    }
+
+    @Override
+    public Answer getAnswer(Token userToken, long areaId, long questionId, long answerId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+        return null;
+    }
+
+    @Override
+    public void updateArea(Token userToken, Area area) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+
+    }
+
+    @Override
+    public void updateQuestion(Token userToken, long areaId, Question question) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+
+    }
+
+    @Override
+    public void updateAnswer(Token userToken, long areaId, long questionId, Answer answer) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException {
+
+    }
 }
