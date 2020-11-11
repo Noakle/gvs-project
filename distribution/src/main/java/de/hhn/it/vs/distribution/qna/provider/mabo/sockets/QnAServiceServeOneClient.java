5dda3b6b2c34ba8d85f8941e7850ad426e955df4
Leonhard Bosch <lbosch@stud.hs-heilbronn.de>
2020-11-11 17:23:24 +0100

    Added program logic in Client

--- distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/mabo/sockets/QnAServiceServeOneClient.java
+++ distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/mabo/sockets/QnAServiceServeOneClient.java
@@ -1,10 +1,20 @@
 package de.hhn.it.vs.distribution.qna.provider.mabo.sockets;
 
+import de.hhn.it.vs.common.core.usermanagement.Token;
 import de.hhn.it.vs.common.exceptions.IllegalParameterException;
+import de.hhn.it.vs.common.exceptions.InvalidTokenException;
+import de.hhn.it.vs.common.exceptions.ServiceNotAvailableException;
+import de.hhn.it.vs.common.qna.model.Answer;
+import de.hhn.it.vs.common.qna.model.Area;
+import de.hhn.it.vs.common.qna.model.Question;
+import de.hhn.it.vs.common.qna.provider.wnck.WnckQnAService;
+import de.hhn.it.vs.distribution.fdkh.provider.socket.fdkhServiceServeOneClient;
 import de.hhn.it.vs.distribution.sockets.AbstractServeOneClient;
-
+import de.hhn.it.vs.distribution.sockets.Request;
+import de.hhn.it.vs.distribution.sockets.Response;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
 
 public class QnAServiceServeOneClient extends AbstractServeOneClient {
 
@@ -29,6 +39,11 @@ public class QnAServiceServeOneClient extends AbstractServeOneClient {
   public static final String PARAM_ANSWER = "param.answere";
   public static final String PARAM_ANSWER_ID = "param.amswereid";
 
+  private static final org.slf4j.Logger logger =
+      org.slf4j.LoggerFactory.getLogger(fdkhServiceServeOneClient.class);
+
+  WnckQnAService qnAService;
+
   /**
    * Creates new thread to work on a single client request.
    *
@@ -37,7 +52,221 @@ public class QnAServiceServeOneClient extends AbstractServeOneClient {
    * @throws IOException               when problems with the socket connection occur
    * @throws IllegalParameterException when called with null references
    */
-  public QnAServiceServeOneClient(Socket socket, Object service) throws IOException, IllegalParameterException {
+  public QnAServiceServeOneClient(Socket socket, Object service)
+      throws IOException, IllegalParameterException {
     super(socket, service);
+  }
+
+  @Override
+  public void run() {
+    Request request = null;
+    Response response;
+
+    try {
+      request = (Request) in.readObject();
+
+      String methodToCall = request.getMethodToCall();
+      switch (methodToCall) {
+        case CREATE_AREA:
+          response = createArea(request);
+          break;
+
+        case CREATE_QUESTION:
+          response = createQuestion(request);
+          break;
+
+        case CREATE_ANSWER:
+          response = createAnswer(request);
+          break;
+
+        case GET_AREA_IDS:
+          response = getAreaIds(request);
+          break;
+
+        case GET_AREA:
+          response = getArea(request);
+          break;
+
+        case GET_QUESTION_IDS:
+          response = getQuestionIds(request);
+          break;
+
+        case GET_QUESTION:
+          response = getQuestion(request);
+          break;
+
+        case GET_ANSWER_IDS:
+          response = getAnswerIds(request);
+          break;
+
+        case GET_ANSWER:
+          response = getAnswer(request);
+          break;
+
+        case UPDATE_AREA:
+          response = updateArea(request);
+          break;
+
+        case UPDATE_QUESTION:
+          response = updateQuestion(request);
+          break;
+
+        case UPDATE_ANSWER:
+          response = updateAnswer(request);
+          break;
+
+        default:
+          ServiceNotAvailableException noMethodException = new ServiceNotAvailableException(
+              "Method : \"" + methodToCall + "\" is not known!");
+          response = new Response(request, noMethodException);
+      }
+    } catch (Exception e) {
+      response = new Response(request, e);
+    }
+
+    try {
+      out.writeObject(response);
+    } catch (IOException e1) {
+      logger.error("Problem writing the response: " + e1.getMessage());
+    }
+  }
+
+  private Response updateAnswer(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    qnAService.updateAnswer(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (long) request.getParameter(PARAM_QUESTION_ID),
+        (Answer) request.getParameter(PARAM_ANSWER)
+    );
+    return new Response(request);
+  }
+
+  private Response updateQuestion(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    qnAService.updateQuestion(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (Question) request.getParameter(PARAM_QUESTION)
+    );
+    return new Response(request);
+  }
+
+  private Response updateArea(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    qnAService.updateArea(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (Area) request.getParameter(PARAM_AREA)
+    );
+    return new Response(request);
+  }
+
+  private Response getAnswer(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    Answer ans = qnAService.getAnswer(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (long) request.getParameter(PARAM_QUESTION_ID),
+        (long) request.getParameter(PARAM_ANSWER_ID)
+    );
+    Response r = new Response(request);
+    r.setReturnObject(ans);
+
+    return r;
+  }
+
+  private Response getAnswerIds(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    List<Long> l = qnAService.getAnswerIds(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (long) request.getParameter(PARAM_QUESTION_ID)
+    );
+    Response r = new Response(request);
+    r.setReturnObject(l);
+
+    return r;
+  }
+
+  private Response getQuestion(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    Question q = qnAService.getQuestion(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (long) request.getParameter(PARAM_QUESTION_ID)
+    );
+    Response r = new Response(request);
+    r.setReturnObject(q);
+
+    return r;
+  }
+
+  private Response getQuestionIds(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    List<Long> l = qnAService.getQuestionIds(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID)
+    );
+    Response r = new Response(request);
+    r.setReturnObject(l);
+
+    return r;
+  }
+
+  private Response getArea(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    Area ar = qnAService.getArea(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID)
+    );
+    Response r = new Response(request);
+    r.setReturnObject(ar);
+
+    return r;
+  }
+
+  private Response getAreaIds(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    List<Long> l = qnAService.getAreaIds(
+        (Token) request.getParameter(PARAM_USER_TOKEN)
+    );
+    Response r = new Response(request);
+    r.setReturnObject(l);
+
+    return r;
+  }
+
+  private Response createAnswer(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    long t = qnAService.createAnswer(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (long) request.getParameter(PARAM_QUESTION_ID),
+        (Answer) request.getParameter(PARAM_ANSWER));
+    Response r = new Response(request);
+    r.setReturnObject(t);
+
+    return r;
+  }
+
+  private Response createQuestion(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    long t = qnAService.createQuestion(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (long) request.getParameter(PARAM_AREA_ID),
+        (Question) request.getParameter(PARAM_QUESTION));
+    Response r = new Response(request);
+    r.setReturnObject(t);
+    return r;
+  }
+
+  private Response createArea(Request request)
+      throws IllegalParameterException, InvalidTokenException, ServiceNotAvailableException {
+    long t = qnAService.createArea(
+        (Token) request.getParameter(PARAM_USER_TOKEN),
+        (Area) request.getParameter(PARAM_AREA));
+    Response r = new Response(request);
+    r.setReturnObject(t);
+    return r;
   }
 }
