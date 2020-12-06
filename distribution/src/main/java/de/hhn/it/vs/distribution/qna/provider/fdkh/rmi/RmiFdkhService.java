c3f858b6493937a5a925f79d8d04f9bedacc7886
fdomino <fdomino@stud.hs-heilbronn.de>
2020-12-06 11:19:54 +0100

    created Class RmiFdkhService

---
+++ distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/fdkh/rmi/RmiFdkhService.java
@@ -0,0 +1,6 @@
+package de.hhn.it.vs.distribution.qna.provider.fdkh.rmi;
+
+public class RmiFdkhService {
+    private static final org.slf4j.Logger logger =
+            org.slf4j.LoggerFactory.getLogger(RmiFdkhService.class);
+}
