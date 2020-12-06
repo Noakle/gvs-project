4e7cc1ed3f71e85f88cd4a955c7a044c4522f777
fdomino <fdomino@stud.hs-heilbronn.de>
2020-12-06 12:13:30 +0100

    added Remote Exception to RmiFdkhService

--- distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/fdkh/rmi/RmiFdkhService.java
+++ distribution/src/main/java/de/hhn/it/vs/distribution/qna/provider/fdkh/rmi/RmiFdkhService.java
@@ -9,46 +9,47 @@ import de.hhn.it.vs.common.qna.model.Area;
 import de.hhn.it.vs.common.qna.model.Question;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface RmiFdkhService extends Remote {
 
-    public static final String REGISTRY_KEY = "cc.qna";
+    public static final String REGISTRY_KEY = "qna.fdkh";
 
     public long createArea(Token userToken, Area area)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public long createQuestion(Token userToken, long areaId, Question question)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public long createAnswer(Token userToken, long areaId, long questionId, Answer answer)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public List<Long> getAreaIds(Token userToken)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public Area getArea(Token userToken, long areaId)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public List<Long> getQuestionIds(Token userToken, long areaId)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public Question getQuestion(Token userToken, long areaId, long questionId)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public List<Long> getAnswerIds(Token userToken, long areaId, long questionId)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public Answer getAnswer(Token userToken, long areaId, long questionId, long answerId)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public void updateArea(Token userToken, Area area)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public void updateQuestion(Token userToken, long areaId, Question question)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
     public void updateAnswer(Token userToken, long areaId, long questionId, Answer answer)
-            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException;
+            throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException;
 
 }
