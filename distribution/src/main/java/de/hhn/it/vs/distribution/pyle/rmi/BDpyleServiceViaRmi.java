c6bfc7057e0f327b34a9568ac2804252eb86ed7f
peguy p <peguyyopa@yahoo.fr>
2021-01-16 16:49:46 +0100

    rim bugs gelöst

--- distribution/src/main/java/de/hhn/it/vs/distribution/pyle/rmi/BDpyleServiceViaRmi.java
+++ distribution/src/main/java/de/hhn/it/vs/distribution/pyle/rmi/BDpyleServiceViaRmi.java
@@ -1,6 +1,5 @@
 package de.hhn.it.vs.distribution.pyle.rmi;
 
-import com.sun.media.jfxmedia.logging.Logger;
 import de.hhn.it.vs.common.core.usermanagement.Token;
 import de.hhn.it.vs.common.exceptions.IllegalParameterException;
 import de.hhn.it.vs.common.exceptions.InvalidTokenException;
@@ -8,13 +7,14 @@ import de.hhn.it.vs.common.exceptions.ServiceNotAvailableException;
 import de.hhn.it.vs.common.qna.model.Answer;
 import de.hhn.it.vs.common.qna.model.Area;
 import de.hhn.it.vs.common.qna.model.Question;
+import de.hhn.it.vs.common.qna.service.BDQnAService;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.List;
 
-public class BDpyleServiceViaRmi implements RmipyleService {
+public class BDpyleServiceViaRmi implements BDQnAService {
     private static final org.slf4j.Logger logger =
             org.slf4j.LoggerFactory.getLogger(BDpyleServiceViaRmi.class);
     private  String hostname;
@@ -47,130 +47,80 @@ public class BDpyleServiceViaRmi implements RmipyleService {
     @Override
     public long createArea(Token userToken, Area area) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
         long result = 0;
+        result = rmipyleService.createArea(userToken, area);
+        return result;
 
-        try {
-
-            result = rmipyleService.createArea(userToken, area);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return  result;
     }
 
     @Override
     public long createQuestion(Token userToken, long areaId, Question question) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
         long result = 0;
-
-        try {
-
-            result = rmipyleService.createQuestion(userToken, areaId, question);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return  result;
+        result = rmipyleService.createQuestion(userToken, areaId, question);
+        return result;
     }
 
     @Override
     public long createAnswer(Token userToken, long areaId, long questionId, Answer answer) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
         long result = 0;
-
-        try {
-
-            result = rmipyleService.createAnswer(userToken, areaId, questionId, answer);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return  result;
+        result = rmipyleService.createAnswer(userToken, areaId, questionId, answer);
+        return result;
     }
 
     @Override
     public List<Long> getAreaIds(Token userToken) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
-       List<Long> result = null;
-
-        try {
-
-            result = rmipyleService.getAreaIds(userToken);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return  result;
+        List<Long> result = null;
+        result = rmipyleService.getAreaIds(userToken);
+        return result;
     }
 
     @Override
     public Area getArea(Token userToken, long areaId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
         Area result = new Area();
-
-        try {
-
-            result = rmipyleService.getArea(userToken, areaId);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
+        result = rmipyleService.getArea(userToken, areaId);
         return  result;
     }
 
     @Override
     public List<Long> getQuestionIds(Token userToken, long areaId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
         List<Long> result = null;
-
-        try {
-
-            result = rmipyleService.getQuestionIds(userToken, areaId);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return  result;
+        result = rmipyleService.getQuestionIds(userToken, areaId);
+        return result;
     }
 
     @Override
     public Question getQuestion(Token userToken, long areaId, long questionId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
-        return null;
+        Question result = new Question();
+        result = rmipyleService.getQuestion(userToken, areaId, questionId);
+        return result;
     }
 
     @Override
     public List<Long> getAnswerIds(Token userToken, long areaId, long questionId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
         List<Long> result = null;
-
-        try {
-
-            result = rmipyleService.getAnswerIds(userToken, areaId, questionId);
-
-        }catch (Exception ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return  result;
+        result = rmipyleService.getAnswerIds(userToken, areaId, questionId);
+        return result;
     }
 
     @Override
     public Answer getAnswer(Token userToken, long areaId, long questionId, long answerId) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
-        return null;
+        Answer result = new Answer();
+        result = rmipyleService.getAnswer(userToken, areaId, questionId, answerId);
+        return result;
     }
 
     @Override
     public void updateArea(Token userToken, Area area) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
 
+        rmipyleService.updateArea(userToken, area);
     }
 
     @Override
     public void updateQuestion(Token userToken, long areaId, Question question) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
-
+        rmipyleService.updateQuestion(userToken, areaId, question);
     }
 
     @Override
     public void updateAnswer(Token userToken, long areaId, long questionId, Answer answer) throws ServiceNotAvailableException, IllegalParameterException, InvalidTokenException, RemoteException {
-
+        rmipyleService.updateAnswer(userToken, areaId, questionId, answer);
     }
 }
--- distribution/src/test/java/de/hhn/it/vs/distribution/pyle/provider/pyle/rmi/DemoClient.java
+++ distribution/src/test/java/de/hhn/it/vs/distribution/pyle/provider/pyle/rmi/DemoClient.java
@@ -1,30 +1,23 @@
 package de.hhn.it.vs.distribution.pyle.provider.pyle.rmi;
 
 import de.hhn.it.vs.common.core.usermanagement.BDUserManagementService;
-import de.hhn.it.vs.common.core.usermanagement.Token;
 import de.hhn.it.vs.common.core.usermanagement.UserNameAlreadyAssignedException;
 import de.hhn.it.vs.common.core.usermanagement.provider.wnck.bd.WnckUserManagementService;
 import de.hhn.it.vs.common.exceptions.IllegalParameterException;
 import de.hhn.it.vs.common.exceptions.InvalidTokenException;
 import de.hhn.it.vs.common.exceptions.ServiceNotAvailableException;
-import de.hhn.it.vs.common.qna.model.Area;
-import de.hhn.it.vs.common.qna.provider.wnck.WnckQnAService;
 import de.hhn.it.vs.common.qna.service.BDQnAService;
 import de.hhn.it.vs.distribution.core.usermanagement.provider.wnck.sockets.BDUserManagementServiceViaRmi;
 import de.hhn.it.vs.distribution.pyle.rmi.BDpyleServiceViaRmi;
-import de.hhn.it.vs.distribution.pyle.rmi.RmipyleService;
 import de.hhn.it.vs.distribution.qna.QnAServiceDemoClient;
 import de.hhn.it.vs.distribution.testsupport.TestMode;
 
-
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class DemoClient {
     private static final org.slf4j.Logger logger =
             org.slf4j.LoggerFactory.getLogger(DemoClient.class);
-    private RmipyleService rmipyleService;
+    private BDpyleServiceViaRmi rmipyleService;
     private BDUserManagementService bdUserManagementService;
 
 
@@ -40,6 +33,11 @@ public class DemoClient {
                 bdUserManagementService = new WnckUserManagementService();
                // rmipyleService = new WnckQnAService(bdUserManagementService);
                 break;
+            case SOCKET:
+               // userManagementService = new BDUserManagementServiceViaSockets("localhost", 1099);
+
+                break;
+
             case RMI:
                 bdUserManagementService = new BDUserManagementServiceViaRmi("localhost", 1099);
                 rmipyleService = new BDpyleServiceViaRmi("localhost", 1099);
@@ -52,38 +50,11 @@ public class DemoClient {
     }
 
     public static void main(String[] args) throws IllegalParameterException,
-    ServiceNotAvailableException, UserNameAlreadyAssignedException, InvalidTokenException {
-    /*Token token = new Token();
-    Area area = new Area();
-    String host = (args.length < 1) ? null : args[0];
-    try
-    {
-        long response = 0;
-        //Registry registry = LocateRegistry.getRegistry(host);
-        BDpyleServiceViaRmi bDpyleServiceViaRmi = new BDpyleServiceViaRmi("127.0.0.1", 1099);
-       if (bDpyleServiceViaRmi.serviceKommuication()) {
-            response = bDpyleServiceViaRmi.createArea(token, area);
-           System.out.println("Response: "+response);
-       }
-       else
-       {
-           System.out.println("Client is not connect to the service");
-       }
-
-    }catch (RemoteException re)
-    {
-        System.out.println(re.toString());
-    } catch (ServiceNotAvailableException e) {
-        e.toString();
-    } catch (InvalidTokenException e) {
-        e.toString();
-    } catch (IllegalParameterException e) {
-        e.toString();
-    }*/
+            ServiceNotAvailableException, UserNameAlreadyAssignedException, InvalidTokenException, RemoteException {
         DemoClient demoClient = new DemoClient(TestMode.RMI);
         demoClient.runDemo();
     }
-    private void runDemo() throws InvalidTokenException, IllegalParameterException, ServiceNotAvailableException, UserNameAlreadyAssignedException {
+    private void runDemo() throws InvalidTokenException, IllegalParameterException, ServiceNotAvailableException, UserNameAlreadyAssignedException, RemoteException {
         QnAServiceDemoClient commonDemo = new QnAServiceDemoClient();
         commonDemo.runDemo(bdUserManagementService, (BDQnAService) rmipyleService);
     }
